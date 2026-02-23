47751e88402795ad4be80e4437853ea4382d3f80
Jadid Alam <Jadid.alam.08@gmail.com>
2026-02-23 07:27:54 +0000

    added a simple implementation of send_message method

--- src-tauri/src/events/client_events.rs
+++ src-tauri/src/events/client_events.rs
@@ -1,7 +1,11 @@
+use anyhow::Ok;
 use ruma::events::room::message::SyncRoomMessageEvent;
 use tauri::{AppHandle, Emitter};
 use serde::Serialize;
 use tracing::{error, trace};
+use matrix_sdk::ruma::events::room::message::RoomMessageEventContent;
+use matrix_sdk::ruma::TransactionId;
+use ruma::RoomId;
 
 pub struct ClientEvents;
 
@@ -57,6 +61,39 @@ impl ClientEvents {
         if let Err(e) = app_handle.emit("matrix:message", payload) {
             error!("Failed to emit message event: {}", e);
         }
+    }
+
+    /// This function sends a message to a specified room. It constructs the message content 
+    /// and uses the Matrix client to send it. The transaction ID is generated for tracking 
+    /// the message sending process.
+    /// 
+    /// currently it only supports plain text. (as this is not complete, it is not added to the invoke handler yet)
+    pub async fn send_message(room_id: &RoomId, content: String, client: &matrix_sdk::Client) -> bool {
+
+        // Message content and transaction ID
+        let content = RoomMessageEventContent::text_plain(content);
+        let txn_id = TransactionId::new();
+
+        // Here we attempt to get the room.
+        if let Some(room) = client.get_room(&room_id) {
+
+            // We attempt to send the message to the room.
+            let result = room.send(content).with_transaction_id(txn_id);
+            
+            // If we failed to send the message, we log the error and return false.
+            if let Err(e) = result.await {
+                error!("Failed to send message: {}", e);
+                return false;
+            }
+
+        } else {
+            // if the room is not found, we log an error and return false.
+            error!("Room with ID {} not found", room_id);
+            return false;
+        }
+
+        // If the message is sent successfully, we return true.
+        return true;
     }
 }
 
