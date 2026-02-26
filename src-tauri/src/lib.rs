d191ac1701881ae4c5a9c3f1a4ec2f725394dcbf
Yahya Ahmad <me@yahya-ahmad.com>
2026-02-26 14:05:54 +0000

    set up basic keyring storage for the stronghold password (tested on linux only so far)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,9 @@ use tauri::Manager;
 use tokio::runtime::Runtime;
 use tokio::sync::RwLock;
 use crate::user::{login, logout, register, reset_account, restore_session};
+use keyring::{Entry, credential};
+use getrandom;
+use hex;
 
 mod user;
 mod client_handler;
@@ -22,6 +25,20 @@ pub fn run() {
             let app_handle = app.handle().clone();
             let client = rt.block_on(ClientHandler::new(app_handle));
             let client_state = ClientState(RwLock::new(Some(client)));
+
+            // create new entry object for the stronghold key
+            let entry = Entry::new("echelon", "stronghold-key").unwrap();
+
+            // if a credential does not exist on the OS, then create one
+            match entry.get_password() {
+                Err(keyring::Error::NoEntry) => {
+                    // generate a random password in keyring
+                    let mut buf = [0u8; 32];
+                    getrandom::fill(&mut buf).expect("Failed to generate a random password for keyring");
+                    let _ = entry.set_password(&hex::encode(buf));
+                },
+                _ => println!("Keyring password already set"),
+            }
 
             app.manage(client_state);
             Ok(())
