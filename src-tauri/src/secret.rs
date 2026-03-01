c8179b3089bff2bff9921a82c72f9f920aa5d914
Yahya Ahmad <me@yahya-ahmad.com>
2026-03-01 21:24:01 +0000

    fixing bits of code in that doesn't need to exist in secret.rs ig

--- src-tauri/src/secret.rs
+++ src-tauri/src/secret.rs
@@ -1,6 +1,7 @@
 use iota_stronghold::{KeyProvider, Stronghold, SnapshotPath, ClientError, Store};
 use keyring::{Entry, Error::NoEntry};
 use std::path::{PathBuf};
+use anyhow::Result;
 use blake3;
 
 pub struct SecretService {
@@ -13,37 +14,6 @@ struct ClientStore {
     key_provider: KeyProvider,
     snapshot_path: SnapshotPath,
     store: Option<Store>,
-}
-
-pub enum SecretError {
-    Keyring(keyring::Error),
-    Random(getrandom::Error),
-    Stronghold(ClientError),
-    Utf8(std::string::FromUtf8Error),
-}
-
-impl From<keyring::Error> for SecretError {
-    fn from(e: keyring::Error) -> Self {
-        SecretError::Keyring(e)
-    }
-}
-
-impl From<getrandom::Error> for SecretError {
-    fn from(e: getrandom::Error) -> Self {
-        SecretError::Random(e)
-    }
-}
-
-impl From<ClientError> for SecretError {
-    fn from(e: ClientError) -> Self {
-        SecretError::Stronghold(e)
-    }
-}
-
-impl From<std::string::FromUtf8Error> for SecretError {
-    fn from(e: std::string::FromUtf8Error) -> Self {
-        SecretError::Utf8(e)
-    }
 }
 
 impl SecretService {
@@ -65,7 +35,7 @@ impl SecretService {
         SnapshotPath::from_path(path)
     }
 
-    fn get_key_provider(&self) -> Result<KeyProvider, SecretError> {
+    fn get_key_provider(&self) -> Result<KeyProvider> {
         let entry = Entry::new(&self.keyring_service, &self.keyring_user)?;
 
         match entry.get_password() {
@@ -73,7 +43,7 @@ impl SecretService {
                 println!("Creating new keyring password");
                 let mut buf = [0u8; 32];
                 getrandom::fill(&mut buf)?;
-                let _ = entry.set_password(&hex::encode(buf));
+                entry.set_password(&hex::encode(buf))?;
             },
             Ok(_) => {
                 println!("Keyring password set")
@@ -87,7 +57,7 @@ impl SecretService {
         Ok(KeyProvider::with_passphrase_hashed_blake2b(keyring_password)?)
     }
 
-    fn get_client_store(&self, username: &String, stronghold: &Stronghold) -> Result<ClientStore, SecretError> {
+    fn get_client_store(&self, username: &String, stronghold: &Stronghold) -> Result<ClientStore> {
         let key_provider = self.get_key_provider()?;
         let snapshot_path = self.get_snapshot_path(&username);
         let snapshot = stronghold.load_snapshot(&key_provider, &snapshot_path);
@@ -119,7 +89,7 @@ impl SecretService {
         Ok(ClientStore { key_provider, snapshot_path, store: Option::from(store) })
     }
 
-    fn set_client_store(&self, username: &String, stronghold: &Stronghold) -> Result<ClientStore, SecretError> {
+    fn set_client_store(&self, username: &String, stronghold: &Stronghold) -> Result<ClientStore> {
         match self.get_client_store(&username, stronghold)? {
             client_store @ ClientStore { store: Some(_), .. } => {
                 Ok(client_store)
@@ -132,7 +102,7 @@ impl SecretService {
         }
     }
 
-    pub fn set_login_tokens(&self, username: String, access_token: String, refresh_token: String) -> Result<(), SecretError> {
+    pub fn set_login_tokens(&self, username: String, access_token: String, refresh_token: String) -> Result<()> {
         let stronghold = Stronghold::default();
         let client_store = self.set_client_store(&username, &stronghold)?;
         let store = client_store.store.unwrap();
@@ -146,23 +116,18 @@ impl SecretService {
         Ok(())
     }
 
-    pub fn get_login_tokens(&self, username: String) -> Result<Option<(String, String)>, SecretError> {
+    pub fn get_login_tokens(&self, username: String) -> Result<Option<(String, String)>> {
         let stronghold = Stronghold::default();
         match self.get_client_store(&username, &stronghold)? {
-            client_store @ ClientStore { store: Some(_), .. } => {
-                let store = match client_store.store {
-                    Some(store) => store,
-                    None => return Ok(None),
-                };
+            ClientStore { store: Some(store), .. } => {
+                let Some(access) = store.get(b"access_token")? else {return Ok(None);};
+                let Some(refresh) = store.get(b"refresh_token")? else {return Ok(None);};
+                let access_token = String::from_utf8(access)?;
+                let refresh_token = String::from_utf8(refresh)?;
 
-                let access_token = String::from_utf8(store.get(b"access_token")?.unwrap());
-                let refresh_token = String::from_utf8(store.get(b"refresh_token")?.unwrap());
-
-                Ok(Some((access_token?, refresh_token?)))
+                Ok(Some((access_token, refresh_token)))
             },
-            ClientStore { store: _, .. } => {
-                Ok(None)
-            }
+            ClientStore { store: _, .. } => { Ok(None) }
         }
     }
 }
